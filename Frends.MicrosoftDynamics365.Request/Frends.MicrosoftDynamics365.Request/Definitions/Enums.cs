a630e0ae3bcba278b03ab14ef4c234ae2aa85176
jefim <hi@jef.im>
2024-06-13 12:42:31 +0300

    Update gitignore, test files

---
+++ Frends.MicrosoftDynamics365.Request/Frends.MicrosoftDynamics365.Request/Definitions/Enums.cs
@@ -0,0 +1,15 @@
+﻿namespace Frends.MicrosoftDynamics365.Request.Definitions;
+
+// Self-explanatory enums
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable SA1602 // Enumeration items should be documented
+public enum Method
+{
+    GET,
+    POST,
+    PATCH,
+    PUT,
+    DELETE,
+}
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning restore SA1602 // Enumeration items should be documented
